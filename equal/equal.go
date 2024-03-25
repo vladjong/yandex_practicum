0e8b57aebf1dd4760b54ea001f6ff78b6a367f6c
Vladislav Gaydenko <vagaydenko@avito.ru>
2024-03-25 22:54:32 +0700

    Add func Equal

---
+++ equal/equal.go
@@ -0,0 +1,5 @@
+package equal
+
+func Equal(a, b int) bool {
+	return a == b
+}
---
+++ equal/equal_test.go
@@ -0,0 +1,47 @@
+package equal
+
+import "testing"
+
+func TestEqual(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				a: 5,
+				b: 5,
+			},
+			want: true,
+		},
+		{
+			name: "fail",
+			args: args{
+				a: 5,
+				b: 10,
+			},
+			want: false,
+		},
+		{
+			name: "temp",
+			args: args{
+				a: 1,
+				b: 10,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Equal(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Equal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
